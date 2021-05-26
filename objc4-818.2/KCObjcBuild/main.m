ebd6aea6a56943a668d2ee82614beec7b8c16a74
邓江洲 <dengjiangzhouReal@gmail.com>
2021-05-26 17:16:21 +0800

    内存对齐，内存的优化重排

--- objc4-818.2/KCObjcBuild/main.m
+++ objc4-818.2/KCObjcBuild/main.m
@@ -36,6 +36,15 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         // 内存对齐
+        
+        
+        // 对象的内存对齐，
+        // 源自于
+        // 结构体的内存对齐
+        
+        
+        
+        
         Soilder *k = [Soilder alloc];
         
         
@@ -68,9 +77,22 @@ int main(int argc, const char * argv[]) {
 
 
 
+// 对象在底层， 编译为结构体
 
 
 
 
 
-// x/4gx
+
+
+
+
+
+// x/4gx， 打印 4 条
+
+
+// x/8gx， 打印 8 条
+
+
+
+// 内存对齐，内存的优化重排
