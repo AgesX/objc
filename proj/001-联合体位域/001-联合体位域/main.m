608938d1a24a5a2f723154b006fa0ad3ed20fba7
邓江洲 <dengjiangzhouReal@gmail.com>
2021-06-01 17:50:29 +0800

    指针指向的内容地址也不同

--- proj/001-联合体位域/001-联合体位域/main.m
+++ proj/001-联合体位域/001-联合体位域/main.m
@@ -17,19 +17,59 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
 
         
+        //  值拷贝
+        
+        // 普通指针
+        
+        
+        // a 是变量
+        int a = 10;
+        int b = 10;
+        
+        // 值相同，地址不同
+        
+        
+        // 这里有一个，值拷贝
+        
+        
+        
+        NSLog(@"%d -- %p", a, &a);
+        
+        NSLog(@"%d -- %p", b, &b);
+        
+        // 栈上，地址是连续的
+        
+        
+        
+        
+        //////
+        
+        
+        
+        
+        // 可能深拷贝
+        // 内存，内容拷贝
         
         
         
         
         
-        
-        
-        
-        
-        
-        
+        // car 是指针
         
         Bicycle *car = [[Bicycle alloc] init];
+        
+        Bicycle *rocket = [[Bicycle alloc] init];
+        
+        // 指针地址不同，指针指向的内容地址也不同
+        NSLog(@"%@ -- %p", car, &car);
+        
+        NSLog(@"%@ -- %p", rocket, &rocket);
+        
+        
+        
+        //////
+        
+        
         
         NSLog(@"isBack:             %@", [car isBack]  ? @"TRUE": @"FALSE");
         
