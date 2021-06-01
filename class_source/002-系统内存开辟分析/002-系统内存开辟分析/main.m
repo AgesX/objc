c3bd179fd7c490d4db503d49a979019baffc2681
邓江洲 <dengjiangzhouReal@gmail.com>
2021-06-01 15:49:47 +0800

    isa    步骤

--- class_source/002-系统内存开辟分析/002-系统内存开辟分析/main.m
+++ class_source/002-系统内存开辟分析/002-系统内存开辟分析/main.m
@@ -13,6 +13,17 @@
 
 
 
+void testOne(void){
+    // 验证，类信息，内存中只存在一份
+    Class one = [Gladiator class];
+    Class two = [Gladiator alloc].class;
+    Class three = object_getClass([Gladiator alloc]);
+    Class four = [Gladiator alloc].class;
+    
+    
+    NSLog(@"\n%p - \n%p - \n%p - \n%p", one, two, three, four);
+}
+
 
 
 
@@ -21,18 +32,37 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
             
+        // testOne();
         
         
-        // 验证，类信息，内存中只存在一份
-        Class one = [Gladiator class];
-        Class two = [Gladiator alloc].class;
-        Class three = object_getClass([Gladiator alloc]);
-        Class four = [Gladiator alloc].class;
+        //  testTwo
         
         
-        NSLog(@"\n%p - \n%p - \n%p - \n%p", one, two, three, four);
+        // NSObject 实例对象
+        
+        NSObject * obj = [NSObject alloc];
+        
+        // NSObject 类
+        
+        Class cls = object_getClass(obj);
+        
+        // NSObject 元类 , 即 根元类
+        
+        Class metaCls = object_getClass(cls);
+        
+        // NSObject 根元类，指向自身
+        // 根元类
+        
+        Class rootMetaCls = object_getClass(metaCls);
+        
+        
+        // NSObject 根元类，指向自身
+        // 根根元类
     
+        Class rootRootMetaCls = object_getClass(rootMetaCls);
         
+        
+        NSLog(@"\n实例对象  %p \n类  %p \n元类  %p \n根元类    %p \n根根元类    %p ", obj, cls, metaCls, rootMetaCls, rootRootMetaCls);
         
         
     }
