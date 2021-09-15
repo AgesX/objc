652990e50765e2acabb36c206bf8783f1c6ad742
邓江洲 <dengjiangzhouReal@gmail.com>
2021-09-16 04:30:58 +0800

    sample one

---
+++ 逆向/proj/one/one/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  one
+//
+//  Created by Jz D on 2021/9/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 逆向/proj/one/one/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  one
+//
+//  Created by Jz D on 2021/9/16.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+
+
+
+int test(void){
+    
+    int temp = 0xFFFFFFFF;
+    return temp;
+
+}
+
+
+
+
+
+
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    printf("%x\n", test());
+    
+    
+    
+    
+}
+
+
+@end
