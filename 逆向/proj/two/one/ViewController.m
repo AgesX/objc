b6b2976bfc479a101dc7fc80757daf59d9d4a93b
邓江洲 <dengjiangzhouReal@gmail.com>
2021-09-30 10:49:09 +0800

    stp x0, x1, [sp]                              // 从 sp 指向的区域，放数据

---
+++ 逆向/proj/two/one/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  one
+//
+//  Created by Jz D on 2021/9/16.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+
+// 调用汇编
+
+// 函数的声明
+
+int B();
+
+
+int test(void){
+    
+    int temp = 0x1FFFFFFFF;
+    
+    
+    return temp;
+
+}
+
+
+
+
+
+
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    test();
+    printf("%x\n", test());
+    
+    
+    B();
+    
+}
+
+
+@end
+
+
+
+
