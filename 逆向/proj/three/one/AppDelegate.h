8ba809dcd91c2dc1c7d1b9dc0b821ae85a28913d
邓江洲 <dengjiangzhouReal@gmail.com>
2021-10-11 08:47:40 +0800

    +ret 命令， + + +只会看你的 lr 寄存器

---
+++ 逆向/proj/three/one/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  one
+//
+//  Created by Jz D on 2021/9/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 逆向/proj/three/one/ViewController.m
@@ -0,0 +1,57 @@
+//
+//  ViewController.m
+//  one
+//
+//  Created by Jz D on 2021/9/16.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+
+// 调用汇编
+
+// 函数的声明
+int A();
+int B();
+
+
+int test(void){
+    
+    int temp = 0x1FFFFFFFF;
+    
+    
+    return temp;
+
+}
+
+
+
+
+
+
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    printf("开始 \n");
+    
+    
+    A();        // OC 调用汇编
+    
+    
+    printf("结束 \n");
+    
+}
+
+
+@end
+
+
+
+
